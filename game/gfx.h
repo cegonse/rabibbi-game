480cfc21f22f406d34514c64b5fdc58683d76ca6
César González (CI) <cegonse@gmail.com>
2017-10-18 15:46:32 +0000

    *** CI *** Automated ROM build

--- game/gfx.h
+++ game/gfx.h
@@ -4,7 +4,3 @@
 extern const SpriteDefinition character_def;
 extern const Palette tiles_pal_def;
 extern const TileSet basic_tiles_def;
-extern const Palette rabbibi_tiles_pal_def;
-extern const TileSet rabbibi_tiles_def;
-
-#endif // __GFX_H_
